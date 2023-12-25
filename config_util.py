03aac0fa3ca5b6475530cafae7af72db163f67bc
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2023-12-25 16:13:15 +0530

    updating params

--- config_util.py
+++ config_util.py
@@ -1,6 +1,14 @@
 import os
 from configparser import ConfigParser
 
+from fastapi import HTTPException, status
+
+from logger import logger
+
+config_file_path = 'config.ini'  # Update with your config file path
+config = ConfigParser()
+config.read(config_file_path)
+
 
 def get_config_value(section, key, default=None):
     # Check if the key exists in the environment variables
@@ -8,14 +16,13 @@ def get_config_value(section, key, default=None):
 
     # If the key is not in the environment variables, try reading from a config file
     if value is None:
-        config_file_path = 'config.ini'  # Update with your config file path
-        config = ConfigParser()
 
         # Attempt to read the config file
         try:
-            config.read(config_file_path)
             value = config.get(section, key, fallback=default)
         except Exception as e:
-            print(f"Error reading config file: {e}")
+            logger.error(
+                {"Exception": f"Error reading config file: {e}"})
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Error while reading configuration: "+key)
 
     return value
