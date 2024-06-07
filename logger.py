37acea9f83f0a3fed467f7aa33b56ddb7b96d29b
Jayaprakash8887 <Jayaprakash.narayanaswamy@tarento.com>
2024-06-07 17:00:52 +0530

    Adding Logger conf

--- logger.py
+++ logger.py
@@ -3,15 +3,10 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-logger_name = "sakhi_activity"
-log_level = os.environ["LOG_LEVEL"]
-log_format = '%(asctime)s - %(thread)d - %(threadName)s - %(name)s - %(levelname)s - %(message)s'
 
-logging.basicConfig(
-    level=log_level,
-    format=log_format,
-    datefmt='%Y-%m-%d %H:%M:%S',
-)
+logging.config.fileConfig('logging.conf')
+
+logger_name = "app"
 
 # Configure the logger
 logger = logging.getLogger(logger_name)
